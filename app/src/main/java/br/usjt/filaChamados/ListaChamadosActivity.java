bf7de48464cb636406a5214cdef6a82f78f916cd
arqdsis <arqdsis@MOOLI10306.animaedu.intranet>
2019-03-20 20:59:41 -0300

    Comite teste

---
+++ app/src/main/java/br/usjt/filaChamados/ListaChamadosActivity.java
@@ -0,0 +1,42 @@
+package br.usjt.filaChamados;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListaChamadosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_chamados);
+
+    }
+    public List<String> geraListaChamados(){
+        ArrayList<String> lista = new ArrayList<>();
+        lista.add("Desktops: Computador da secretária quebrado.");
+        lista.add("Telefonia: Telefone não funciona.");
+        lista.add("Redes: Manutenção no proxy.");
+        lista.add("Servidores: Lentidão generalizada.");
+        lista.add("Novos Projetos: CRM");
+        lista.add("Manutenção Sistema ERP: atualizar versão.");
+        lista.add("Novos Projetos: Rede MPLS");
+        lista.add("Manutenção Sistema de Vendas: incluir pipeline.");
+        lista.add("Manutenção Sistema ERP: erro contábil");
+        lista.add("Novos Projetos: Gestão de Orçamento");
+        lista.add("Novos Projetos: Big Data");
+        lista.add("Manoel de Barros");
+        lista.add("Redes: Internet com lentidão");
+        lista.add("Novos Projetos: Chatbot");
+        lista.add("Desktops: troca de senha");
+        lista.add("Desktops: falha no Windows");
+        lista.add("Novos Projetos: ITIL V3");
+        lista.add("Telefonia: liberar celular");
+        lista.add("Telefonia: mover ramal");
+        lista.add("Redes: ponto com defeito");
+        lista.add("Novos Projetos: ferramenta EMM");
+        return lista;
+    }
+}
---
+++ app/src/main/java/br/usjt/filaChamados/MainActivity.java
@@ -0,0 +1,60 @@
+package br.usjt.filaChamados;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText nomeFilaEditText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        nomeFilaEditText = findViewById(R.id.nomeFilaEditText);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String nomeFila = nomeFilaEditText.getEditableText().toString();
+                Intent intent = new Intent(MainActivity.this, ListaChamadosActivity.class);
+                intent.putExtra("nome_fila", nomeFila);
+                startActivity(intent);
+
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
